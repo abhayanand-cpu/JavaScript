970fc700bbc1e5266f2c9710cb9d3037fa692af2
student <sidd.anand2000@gmail.com>
2024-02-13 10:54:03 +0530

    Day2 javascript

---
+++ object.js
@@ -0,0 +1,41 @@
+let a = {
+    name: "Deepu",
+    age: 23,
+    score: 7.7
+};
+
+console.log(typeof(a));
+
+let book1 = {
+    name: "Order and chaos",
+    author:"Jorden Peterson",
+    publisher:"Random House Canada Penguin Allen Lane (UK)"
+};
+
+let book2 = {
+    name: "Order and chaos",
+    author:"Jorden Peterson",
+    publisher:"Random House Canada Penguin Allen Lane (UK)"
+};
+
+let book3 = {
+    name: "Order and chaos",
+    author:"Jorden Peterson",
+    publisher:"Random House Canada Penguin Allen Lane (UK)"
+};
+
+let book4 = {
+    name: "dialogues of plato",
+    author:"plato",
+    publisher:"Random House Canada Penguin Allen Lane (UK)"
+};
+
+let book5 = {
+    name: "Order and chaos",
+    author:"Jorden Peterson",
+    publisher:"Random House Canada Penguin Allen Lane (UK)"
+};
+
+// console.log(book1.name);
+
+console.log("the title of the book is "+book2.name+" and the author is "+book2.author+".");
---
+++ object2.js
@@ -0,0 +1,146 @@
+let student = {
+    name : "Abhayanand Siddharth",
+    mobile: 532154842,
+    semester: 5
+};
+console.log(student);
+student.semester = 6;
+console.log(student);
+
+let player = {
+    Name: "R.Pragnananda",
+    Score: 5.5
+};
+
+player.Maximum_Score = 7.5;
+console.log("Player has Scored "+player.Score+" and the maimum score of the player is "+player.Maximum_Score);
+
+function addTwoNumber(){
+    
+    let a = 10;
+    let b = 20;
+    console.log(a+b);
+
+}   
+addTwoNumber();
+
+function addTwoNumber(x,y){
+    
+    let z = x+y;
+    
+    console.log(z);
+
+}
+addTwoNumber(10,20);
+
+let obj1 = {
+    price: 45,
+    tax: 5
+};
+
+let obj2 = {
+    price: 50,
+    tax: 5
+};
+
+let obj3 = {
+    price: 55,
+    tax: 5
+};
+
+let obj4 = {
+    price: 95,
+    tax: 5
+};
+
+let obj5 = {
+    price: 85,
+    tax: 5
+};
+
+function finalPrice(x){
+    let final_Price = x.price + ( x.price * x.tax / 100 );
+    console.log("final price is "+final_Price);
+}
+
+finalPrice(obj1);
+finalPrice(obj2);
+finalPrice(obj3);
+finalPrice(obj4);
+finalPrice(obj5);
+
+let movie1 = {
+    title : "Haseen Dillruba",
+    actors: "Taapsee Pannu",
+    directors: "Vinil Mathew"
+};
+
+let movie2 = {
+    title : "12th Fail",
+    actors: "Vikrant Massey",
+    directors: "Vidhu Vinod Chopra"
+};
+
+let movie3 = {
+    title : "Super 30",
+    actors: "Hrithik Roshan",
+    directors: "Vikas Bahl"
+};
+
+let movie4 = {
+    title : "3 Idiots",
+    actors: "Amir khan, R madhwan , Sharman Joshi",
+    directors: "Raj kumar irani"
+};
+
+let movie5 = {
+    title : "Munna Bhai M.B.B.S.",
+    actors: "Sanju baba",
+    directors: "Rajkumar Hirani"
+};
+
+function movies( obj ){
+    console.log("---------------------------------------------------------------------------");
+    console.log("movie = "+obj.title+" ,actor = "+obj.actors+" ,director = "+obj.directors);
+    console.log("---------------------------------------------------------------------------");
+}
+
+movies(movie1);
+movies(movie2);
+movies(movie3);
+movies(movie4);
+movies(movie5);
+
+//multiplication, addition, substraction, division using js function
+
+function addition( x, y ){
+    return x + y;
+}
+
+function substraction( x, y ){
+    return x - y;
+}
+
+function multiplication( x, y ){
+    return x * y;
+}
+
+function division( x, y ){
+    return x / y;
+}
+
+let x = prompt("Enter 1st digit");
+let y = prompt("Enter 2nd digit");
+
+x = parseInt(x);
+y = parseInt(y)
+
+let add = addition(x,y);
+let subs = substraction( x, y );
+let mul = multiplication( x, y );
+let div = division( x, y );
+
+console.log("addition of "+x+" and "+y+" is "+ add);
+console.log("substraction of "+x+" and "+y+" is "+ subs);
+console.log("multiplication of "+x+" and "+y+" is "+ mul);
+console.log("division of "+x+" and "+y+" is "+ div);
