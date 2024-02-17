4ff63fac300b53b265cb7f4a6347c210791bfeb5
student <sidd.anand2000@gmail.com>
2024-02-17 12:03:34 +0530

    day3

---
+++ javaScript/index.js
@@ -0,0 +1,10 @@
+// const click = document.getElementById("Calculate");
+
+// addEventListener("click", initial);
+function Clc(){
+        const income = document.getElementById("income").value;
+        const housePropety = document.getElementById("incomeHouse").value;
+        const otherSources = document.getElementById("incomeSoueces").value;
+    
+        console.log(income);
+}
