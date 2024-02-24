bffffa1b92cd4bdb25f53fb7fbd81b32eedf0b62
student <sidd.anand2000@gmail.com>
2024-02-24 11:51:34 +0530

    dya6

--- javaScript/arrays.js
+++ javaScript/arrays.js
@@ -134,3 +134,94 @@ z(b)
     z(store3);
 }
 
+//MAP
+/*a.map(function functionName( value, index, array ){
+    return (can return expression on array of each element with value and index as per use );
+});*/
+z("MAP")
+{
+    let a = [2,6,3,9,5];
+    z(a);
+
+    let arr = a.map( function mul(x){
+        return x * 2;
+    });
+
+    z(arr);
+}
+
+user = [
+    {firstName:"Abhayanand",lastName:"Siddharth"},
+    {firstName:"Priyoranjan",lastName:"Khatua"},
+];
+
+let fullName = user.map( function(value, index, Array){
+    return Array[index].firstName+" "+Array[index].lastName;
+});
+
+z(fullName);
+
+{
+    let a = [3,5,2,7,4];
+
+    let fa = a.map( (value) => {
+        let ans=1;
+
+        while( value != 0 ){
+            ans *= value;
+            value--; 
+        }
+
+        return ans;
+
+    });
+
+    z(fa);
+}
+//FILTER METHOD(RETURNS THE ARRAY THAT PASSES THE TEST)
+z("filter");
+{
+    let arr = [7,9,7,8,53,7,57,3,87];
+
+    let pass = arr.filter( function trueofNot(x) {
+        return x > 7;
+    });
+
+    z(pass);
+
+    var stu = [
+        {rollNo:3,Name:"Abhay",Marks:61},
+        {rollNo:8,Name:"Abhayanand",Marks:45},
+        {rollNo:5,Name:"Abhayanand Siddharth",Marks:65},
+        {rollNo:6,Name:"Priyo",Marks:77},
+        {rollNo:4,Name:"Priyoranjan Khatua",Marks:75},
+        {rollNo:9,Name:"Priyoranjan",Marks:78},
+    ]
+
+    let passingStudent = stu.filter( (obj) => {
+        if( obj.Marks > 60 && obj.rollNo >3 )
+            return obj;
+    });
+
+    z(passingStudent);
+}
+
+{
+    let a = [5,3,6,8,4,5,2,7,4,9,12];
+
+    let re = a.reduce( function red(acc,curr){
+        acc += curr;
+        return acc;
+    });
+    
+    z(re);
+
+    let s = stu.reduce( function re(sum,curr){
+        sum += curr.Marks;
+        return sum;
+    },0);
+    z(s);
+}
+
+
+
