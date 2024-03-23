800f7b4e78af9e8b951486e0245ff4af9ad283a9
abhayanand-cpu <sidd.anand2000@gmail.com>
2024-03-23 11:58:16 +0530

    todo

--- javaScript/task.js
+++ javaScript/task.js
@@ -1,15 +1,42 @@
-const task = document.getElementById("addTask");
+const fetchtask = document.getElementById("addTask");
 const container = document.getElementById("task-container");
 const date = document.getElementById("date");
+const content = document.getElementById("taskDes");
+
 const d = new Date();
 
-task.addEventListener("click",()=>{
+fetchtask.addEventListener("click",()=>{
     let date_value = date.value;
 
     if( date_value != "")
         container.style.display = "flex";
-
 });
+
+function confirmReq(){
+
+
+    if( content.value != "" ){
+        let div = document.createElement('div');
+        document.body.append(div);
+        div.setAttribute("class","addedTask");
+        let child = document.createElement('div');
+        div.appendChild(child);
+        child.setAttribute("id","time");
+        let child2 = document.createElement('div');
+        div.appendChild(child2);
+        child.setAttribute("id","description");       
+        
+    }
+    const descri = document.getElementById("description");
+    const fetchTime = document.getElementById("time");
+    descri.innerText = content.value;
+
+    let h = d.getHours();
+    let m = d.getMinutes();
+    let s = d.getSeconds();
+
+    fetchTime.innerText = date.value+":"+h+":"+m+":"+s;
+}
 
 
 
